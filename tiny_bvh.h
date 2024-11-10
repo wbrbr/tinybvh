8b0895a6adfdf3cbfbc116c1880ad97bb06d625c
Jacco Bikker <bikker.j@gmail.com>
2024-11-10 13:19:31 +0100

    Updated version number.

--- tiny_bvh.h
+++ tiny_bvh.h
@@ -75,7 +75,7 @@ THE SOFTWARE.
 // library version
 #define TINY_BVH_VERSION_MAJOR	0
 #define TINY_BVH_VERSION_MINOR	4
-#define TINY_BVH_VERSION_SUB	1
+#define TINY_BVH_VERSION_SUB	2
 
 // ============================================================================
 //
